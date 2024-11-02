37db5913f156f5274eda434903d752ffee80ac66
Sumit-hubgit <119445835+Sumit-hubgit@users.noreply.github.com>
2024-11-02 17:06:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
+import path from "path";
+import react from "@vitejs/plugin-react";
 import eslint from 'vite-plugin-eslint';
-import { defineConfig } from "vite"
- 
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react(),eslint()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    port: '0.0.0.0', // Set your desired port here
+    strictPort: true, // Fails if the port is already in use
+  },
+});
+
